d324d1ce1ef499133057468d6bc0f85eb1b8535d
halacrcrc <2253453812q@gmail.com>
2022-09-03 16:55:38 +0800

    add delete argument

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{env, process};
+use minigrep::{Config, run};
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err); 
+        process::exit(0);
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {}",e);
+        process::exit(0);
+    }
+}
+
